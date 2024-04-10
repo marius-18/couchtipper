6704af95c03c679fade3de8d55576e41bd367290
Marius <marius@ing-hagemann.de>
2024-04-10 23:50:35 +0200

    fixed table names

--- src/include/lib/precomputation.inc.php
+++ src/include/lib/precomputation.inc.php
@@ -1,12 +1,10 @@
 <?php 
-
+## TODO: Also Precompute for all Goals and for BuLi
 
 function precompute_all_tipps_to_db($modus){
-    ## TODO: auslagern zu Precomputation
-    ## das dann von der API ausführen lassen.. alle paar minuten.., aber nur wenn Wettbewerb aktiv ist
-    ## Speichert das "other Tipps" Array in die Datenbank.. 
+    ## Berechnet das "other Tipps" Array und speichert es in die Datenbank.. 
     global $g_pdo;
-    list($other_tipps_args, $punkte) =  get_other_tipps(0, $modus);
+    list($other_tipps_args, $punkte) = get_other_tipps(0, $modus);
     
     $max_string_length = 1024;
 
@@ -14,7 +12,7 @@ function precompute_all_tipps_to_db($modus){
     $split_array = str_split($help, $max_string_length );
     
     ## Zuerst wird die DB geleert..
-    $sql = "TRUNCATE `Precomputation`";
+    $sql = "TRUNCATE `Precompute_Tipps`";
     $result = $g_pdo->query($sql);
     
     if (!$result) {
@@ -25,7 +23,7 @@ function precompute_all_tipps_to_db($modus){
     $input_error = false;
     ## Jetzt durch alle Elemente gehen und in die DB speichern
     foreach ($split_array as $key => $val){
-        $stmt = $g_pdo->prepare("INSERT INTO `Precomputation`(`id`, `value`) VALUES (:key, :val)");
+        $stmt = $g_pdo->prepare("INSERT INTO `Precompute_Tipps`(`id`, `value`) VALUES (:key, :val)");
         $params = array('key' => $key, 'val' => $val);
         $stmt->execute($params);
         if (!$stmt){
@@ -42,10 +40,9 @@ function precompute_all_tipps_to_db($modus){
 
 
 function get_precompute_tipps(){
-    ## TODO: Auslagern zu Precomputation
     global $g_pdo;
     
-    $stmt = $g_pdo->prepare("SELECT `id`, `value` FROM `Precomputation` WHERE 1 ORDER BY id ASC");
+    $stmt = $g_pdo->prepare("SELECT `id`, `value` FROM `Precompute_Tipps` WHERE 1 ORDER BY id ASC");
     $stmt->execute();
     $string = "";
     foreach ($stmt as $entry) { 
